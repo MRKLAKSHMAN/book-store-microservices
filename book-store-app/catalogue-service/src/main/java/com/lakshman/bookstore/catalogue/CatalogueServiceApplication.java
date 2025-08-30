defcf54acbcd539017bdfd26602de0830ce4f3f8
MRKLAKSHMAN <lakshmankumar329@gmail.com>
2025-08-30 18:35:53 +0530

    MVNW made executable

--- book-store-app/catalogue-service/src/main/java/com/lakshman/bookstore/catalogue/CatalogueServiceApplication.java
+++ book-store-app/catalogue-service/src/main/java/com/lakshman/bookstore/catalogue/CatalogueServiceApplication.java
@@ -7,7 +7,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class CatalogueServiceApplication {
 
     public static void main(String[] args) {
-
         SpringApplication.run(CatalogueServiceApplication.class, args);
     }
 }
