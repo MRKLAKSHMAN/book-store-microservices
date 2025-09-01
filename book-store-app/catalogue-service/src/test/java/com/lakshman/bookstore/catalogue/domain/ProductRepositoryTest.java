3277dbcc06920bd15e60b9a8160506dc58907189
MRKLAKSHMAN <lakshmankumar329@gmail.com>
2025-09-01 19:47:19 +0530

    Slice Test written for findAll Products Repository method

---
+++ book-store-app/catalogue-service/src/test/java/com/lakshman/bookstore/catalogue/domain/ProductRepositoryTest.java
@@ -0,0 +1,30 @@
+package com.lakshman.bookstore.catalogue.domain;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.util.List;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.test.context.jdbc.Sql;
+
+@DataJpaTest(
+        properties = {
+            "spring.test.database.replace=none",
+            "spring.datasource.url=jdbc:tc:postgresql:16-alpine:///db",
+        })
+// Slice Test(Focused On Repository layer) alone
+@Sql("/test-data.sql")
+// @Import(TestcontainersConfiguration.class)
+class ProductRepositoryTest {
+
+    @Autowired
+    private ProductRepository productRepository;
+
+    @Test
+    void shouldGetAllProducts() {
+        List<ProductEntity> products = productRepository.findAll();
+        assertThat(products).hasSize(14);
+    }
+}
